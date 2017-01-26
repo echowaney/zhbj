bbe9beb73eaec8f6c8568677ecf2c9c1d22e3144
echowaney <1194165366@example.com>
2017-01-26 21:19:09 +0800

    分别对viewpager和radiogroup 设置监听,动态的去控制viewpager滑动 时候 radiobutton被选中,radiobutton被点击了 对应的fragment被选中

--- app/src/main/java/com/example/hashwaney/zhbj/activity/MainActivity.java
+++ app/src/main/java/com/example/hashwaney/zhbj/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.hashwaney.zhbj.R;
 import com.example.hashwaney.zhbj.adapter.TabAdapter;
@@ -18,10 +20,17 @@ import java.util.List;
 
 public class MainActivity
         extends AppCompatActivity
+        implements RadioGroup.OnCheckedChangeListener, ViewPager.OnPageChangeListener
 {
 
     private ViewPager mViewPager;
-    private        List<Fragment> fragmentLists =new ArrayList<>();
+    private List<Fragment> fragmentLists = new ArrayList<>();
+    private RadioGroup mRadioGroup;
+    private RadioButton mRb_home;
+    private RadioButton mRb_newscenter;
+    private RadioButton mRb_smartservice;
+    private RadioButton mRb_setting;
+    private RadioButton mRb_govffairs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +43,19 @@ public class MainActivity
 
     private void initView() {
         mViewPager = (ViewPager) findViewById(R.id.vp);
-
+        mRadioGroup = (RadioGroup) findViewById(R.id.rg);
+        mRb_home = (RadioButton) findViewById(R.id.rb_home);
+        mRb_newscenter = (RadioButton) findViewById(R.id.rb_newscenter);
+        mRb_smartservice = (RadioButton) findViewById(R.id.rb_smartservice);
+        mRb_setting = (RadioButton) findViewById(R.id.rb_setting);
+        mRb_govffairs = (RadioButton) findViewById(R.id.rb_govffairs);
+        mRadioGroup.setOnCheckedChangeListener(this);
+//        mViewPager.setOnPageChangeListener();
+        //对viewpager进行页面切换监听
+        mViewPager.addOnPageChangeListener(this);
 
     }
+
     //初始化viepager
     private void initVp() {
         fragmentLists.add(new HomeFragment());
@@ -44,7 +63,70 @@ public class MainActivity
         fragmentLists.add(new SmarServiceFragment());
         fragmentLists.add(new SettingFragment());
         fragmentLists.add(new GovaffairsFragment());
-        TabAdapter adapter =new TabAdapter(getSupportFragmentManager(), fragmentLists);
+        TabAdapter adapter = new TabAdapter(getSupportFragmentManager(), fragmentLists);
         mViewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        //checkId代表了是选中的条目id
+        int item = 0;
+        switch (checkedId) {
+            case R.id.rb_home:
+                item = 0;
+
+                break;
+            case R.id.rb_newscenter:
+                item = 1;
+                break;
+            case R.id.rb_smartservice:
+                item = 2;
+                break;
+            case R.id.rb_setting:
+                item = 3;
+                break;
+            case R.id.rb_govffairs:
+                item = 4;
+                break;
+        }
+
+        //当radiogroup点击选中radiobutton viewpager切换对应的fragment
+        mViewPager.setCurrentItem(item, false);//
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+
+
+    }
+    //在页面被选中的时候进行相应的操作
+    @Override
+    public void onPageSelected(int position) {
+        switch (position) {
+            case 0:
+                mRb_home.setChecked(true);
+                 break;
+            case 1:
+                mRb_newscenter.setChecked(true);
+                break;
+            case 2:
+                mRb_smartservice.setChecked(true);
+                break;
+            case 3:
+                mRb_setting.setChecked(true);
+                break;
+            case 4:
+                mRb_govffairs.setChecked(true);
+                break;
+        }
+
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
---
+++ app/src/main/java/com/example/hashwaney/zhbj/fragment/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.hashwaney.zhbj.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by HashWaney on 2017/1/26.
+ */
+
+public class HomeFragment
+        extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        TextView textView =new TextView(container.getContext());
+        textView.setText("首页");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(20);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+}
